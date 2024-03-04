4785d8ac1c94041e80947993e54600318877ba68
Carlos Campbell <carlos.g.campbell@gmail.com>
2024-03-03 20:19:10 -0500

    cell boxes can be clicked on

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
-//to add notes
+const gameBoard = (() => {
+    let gameboard = ["", "", "", "", "", "", "", "", ""]
+
+    //incompleted. 
+
+    // const render = () => {
+    //    document.querySelector(".gameboard");
+    //    const cells = document.querySelectorAll(".cells");
+    //    cells.forEach((cell) =>{
+    //     cell.addEventListener("click", )
+    //    })
+    // }
+
+    // return {
+    //     render,
+    // }
+
+})
+
+const cellElements = document.querySelectorAll('[data-cell]') //target's every "data-cell" within the html. has nothing to do with the gameboard array within gameBoard
+
+
+cellElements.forEach((cell) => {
+    cell.addEventListener('click', handleClick, {once: true})//this makes it so once it's been clicked, it can't be clicked again
+})
+
+const startGame = document.querySelector("#start-Game");
+startGame.addEventListener("click", () => {
+Game.start();
+})
+
+
+function handleClick(e){
+    console.log("clicked")
+}
+
+
+const Game = (() => { 
+    const start = () => {
+        //first, the gameboard needs to be shown to the players 
+
+        const showGameBoard = document.querySelector("main")
+        showGameBoard.style.display = "block";
+    }
+
+    return{
+        start
+    }
+  
+})();
